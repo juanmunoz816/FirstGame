91764f88c2265565f5087902c8914986e59e32c4
juanmunoz816 <juanmunozrozo@gmail.com>
2023-10-26 19:45:15 -0500

    primer commit

---
+++ game.js
@@ -0,0 +1,11 @@
+const canvas = document.querySelector('#game');
+const game = canvas.getContext('2d');
+
+
+window.addEventListener('load' , startGame)
+
+function startGame(){
+    
+    game.fillRect(0,0,100,100)
+    game.clearRect(0,0,100,50)
+}
