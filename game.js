eedd026bdc20edeb69520c0155981b2e1ec8ef55
juanmunoz816 <juanmunozrozo@gmail.com>
2023-10-27 19:26:01 -0500

    segundo commit

--- game.js
+++ game.js
@@ -1,11 +1,49 @@
 const canvas = document.querySelector('#game');
 const game = canvas.getContext('2d');
 
+let canvasSize;
+let elementsSize;
 
-window.addEventListener('load' , startGame)
+window.addEventListener('load' , setCanvasSize);
+window.addEventListener('resize' ,setCanvasSize)
+
+
+function setCanvasSize(){
+    
+
+    if(window.innerHeight > window.innerWidth){
+        canvasSize = innerWidth*0.7;
+    }else{
+        canvasSize = innerHeight *0.7;
+    }
+
+    canvas.setAttribute('width' , canvasSize);
+    canvas.setAttribute('height', canvasSize);
+    
+    elementsSize = (canvasSize/ 10);
+
+    startGame()
+}
 
 function startGame(){
-    
-    game.fillRect(0,0,100,100)
-    game.clearRect(0,0,100,50)
+
+
+
+    game.font = (elementsSize*0.8) + 'px Roboto Mono';
+    game.textAlign = 'end'
+
+    const map = maps[2]
+    const mapRow = map.trim().split('\n');
+    console.log(mapRow);
+    const mapRowCol = mapRow.map (row => row.trim().split(''));
+
+    let x = elementsSize;
+    let y = elementsSize * 0.98;
+
+    for (let i = 1; i <= 10; i++){
+        for(let j = 1; j <= 10; j++){
+            game.fillText(emojis[mapRowCol[i-1][j-1]] , x *j , y*i  )  
+        }
+        
+    }
 }
